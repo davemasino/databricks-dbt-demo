512176acb2f316692cae6ef2a5aab70cdc71e3f9
Dave Masino <davem@slalom.com>
2023-04-30 16:43:08 -0700

    Fix marts in project yml. Build agg off mart

--- models/marts/agg/customer_orders.sql
+++ models/marts/agg/customer_orders.sql
@@ -1,10 +1,11 @@
 {{ config(materialized='table', file_format='delta') }}  
 
 select
-  {{ dbt_utils.star(from=ref('orders'), except=['o_orderkey', 'o_custkey']) }},
-  {{ dbt_utils.star(from=ref('customer'), except=['c_custkey', 'c_nationkey']) }},
-  {{ dbt_utils.star(from=ref('nation'), except=['n_nationkey', 'n_regionkey']) }}
+  {{ dbt_utils.star(from=ref('fact_orders'), except=['o_orderkey', 'o_custkey']) }},
+  {{ dbt_utils.star(from=ref('dim_customer'), except=['c_custkey']) }},
+  {{ dbt_utils.star(from=ref('dim_order'), except=['o_orderkey']) }}
 from
-  {{ ref('orders') }} as o
-  inner join {{ ref('customer') }} as c on o.o_custkey = c.c_custkey
-  inner join {{ ref('nation') }} as n on c.c_nationkey = n.n_nationkey
+  {{ ref('fact_orders') }} as f
+  inner join {{ ref('dim_customer') }} as c on f.o_custkey = c.c_custkey
+  inner join {{ ref('dim_order') }} as o on f.o_orderkey = o.o_orderkey
+  
