ea85e37234f664829b6fef59ca4bd06e6898311f
Dave Masino <davem@slalom.com>
2023-04-30 16:52:15 -0700

    Fix project yaml again. Build all aggs off star

--- models/marts/agg/orders_by_mktsegment.sql
+++ models/marts/agg/orders_by_mktsegment.sql
@@ -1,10 +1,11 @@
 {{ config(materialized='table', file_format='delta') }}  
 
 select   
-    c_mktsegment,  
+    c.c_mktsegment,  
     count(*) as order_count,  
-    sum(o_totalprice) as total_price  
+    sum(f.o_totalprice) as total_price  
 from   
-    {{ ref("customer_orders") }}  
+  {{ ref('fact_orders') }} as f
+  inner join {{ ref('dim_customer') }} as c on f.o_custkey = c.c_custkey
 group by   
     c_mktsegment 
